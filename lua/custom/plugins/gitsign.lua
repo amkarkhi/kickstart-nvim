46392efe2952a170a043e7e357f46c7738c00f30
amin karkhi <amin.karkhi@digikala.com>
2024-06-29 13:44:45 +0330

    update

--- lua/custom/plugins/gitsign.lua
+++ lua/custom/plugins/gitsign.lua
@@ -2,11 +2,11 @@ return {
     'lewis6991/gitsigns.nvim',
     opts = {
         signs = {
-            add = { hl = 'GitSignsAdd', text = ' ', numhl = 'GitSignsAddNr', linehl = 'GitSignsAddLn' },
-            change = { hl = 'GitSignsChange', text = '', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
-            delete = { hl = 'GitSignsDelete', text = ' ', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
-            topdelete = { hl = 'GitSignsDelete', text = '󱅁 ', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
-            changedelete = { hl = 'GitSignsChange', text = '󰍷 ', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
+            add = { text = ' ' },
+            change = { text = '' },
+            delete = { text = ' ' },
+            topdelete = { text = '󱅁 ' },
+            changedelete = { text = '󰍷 ' },
         },
         signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
         numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
@@ -24,9 +24,7 @@ return {
             delay = 1000,
             ignore_whitespace = false,
         },
-        current_line_blame_formatter_opts = {
-            relative_time = false,
-        },
+        auto_attach = true,
         sign_priority = 6,
         update_debounce = 100,
         status_formatter = nil, -- Use default
@@ -38,9 +36,6 @@ return {
             relative = 'cursor',
             row = 0,
             col = 1,
-        },
-        yadm = {
-            enable = false,
         },
     },
 }
