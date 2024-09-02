520470ef9a58c6cc58aab753bed06a23d4a5ed0f
Amin Karkhi <amkarkhi@gmail.com>
2024-09-02 19:20:26 +0330

    spell check for go

---
+++ after/plugin/spell.lua
@@ -0,0 +1,13 @@
+vim.opt.spell = false
+
+vim.api.nvim_create_autocmd('FileType', {
+    pattern = { 'go', 'markdown', 'text' },
+    callback = function()
+        vim.opt.spell = true
+        vim.opt.spelllang = 'en_us'
+        vim.opt.spelloptions = 'camel'
+        vim.opt.spellcapcheck = ''
+    end,
+})
+
+vim.opt.spellfile = vim.fn.expand '~/.config/nvim/spell/en.utf-8.add'
