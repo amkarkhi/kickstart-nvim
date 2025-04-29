5b7435688cbc160a6723a809675c4cef43727cf0
Amin Karkhi <amkarkhi@gmail.com>
2025-04-29 11:27:17 +0330

    markdown

--- lua/custom/plugins/markdown-preview.lua
+++ lua/custom/plugins/markdown-preview.lua
@@ -1,8 +1,24 @@
 return {
-    'iamcco/markdown-preview.nvim',
-    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
-    ft = { 'markdown' },
-    build = function()
-        vim.fn['mkdp#util#install']()
+    'toppair/peek.nvim',
+    event = { 'VeryLazy' },
+    build = 'deno task --quiet build:fast',
+    config = function()
+        local peek = require 'peek'
+        peek.setup()
+        -- vim.api.nvim_create_user_command('PeekOpen', require('peek').open, {})
+        -- vim.api.nvim_create_user_command('PeekClose', require('peek').close, {})
+        vim.api.nvim_create_user_command('PeekOpen', function()
+            if not peek.is_open() and vim.bo[vim.api.nvim_get_current_buf()].filetype == 'markdown' then
+                vim.fn.system 'i3-msg split horizontal'
+                peek.open()
+            end
+        end, {})
+
+        vim.api.nvim_create_user_command('PeekClose', function()
+            if peek.is_open() then
+                peek.close()
+                vim.fn.system 'i3-msg move left'
+            end
+        end, {})
     end,
 }
