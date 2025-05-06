dcc830c84563e47c3217ff957c32d7d1fc037a8a
Amin Karkhi <amkarkhi@gmail.com>
2025-05-06 20:59:58 +0330

    add distance for ssh but it is not tested

---
+++ lua/custom/plugins/distant.lua
@@ -0,0 +1,9 @@
+return {
+    'chipsenkbeil/distant.nvim',
+    branch = 'v0.3',
+    config = function()
+        require('distant'):setup()
+    end,
+}
+
+-- ssh example.com 'curl -L https://sh.distant.dev | sh -s -- --on-conflict overwrite'
