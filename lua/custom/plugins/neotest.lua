0a2e50d4ce7bf7a22b7a692bec61a0eb8cb96bdf
amin <amin.karkhi@digikala.com>
2025-05-18 15:57:20 +0330

    neotest

--- lua/custom/plugins/neotest.lua
+++ lua/custom/plugins/neotest.lua
@@ -20,7 +20,10 @@ return {
 
         -- add a neovim command to run tests in the current file
         local function run_tests()
-            require('neotest').run.run()
+            local n = require 'neotest'
+            n.output_panel.clear()
+            n.run.run()
+            n.output_panel.open()
         end
         local function run_test_file()
             require('neotest').run.run(vim.fn.expand '%')
@@ -31,14 +34,36 @@ return {
         local function run_test_directory()
             require('neotest').run.run '${workspaceFolder}'
         end
+        local function run_test_summary()
+            require('neotest').summary '${workspaceFolder}'
+        end
+
+        local function run_test_clear_output()
+            require('neotest').output_panel.clear()
+        end
 
         -- register these functions
         vim.api.nvim_create_user_command('NeotestRun', run_tests, { nargs = 0 })
         vim.api.nvim_create_user_command('NeotestRunFile', run_test_file, { nargs = 0 })
         vim.api.nvim_create_user_command('NeotestRunDirectory', run_test_directory, { nargs = 0 })
         vim.api.nvim_create_user_command('NeotestHere', run_my_file, { nargs = 0 })
+        vim.api.nvim_create_user_command('NeotestSummary', run_test_summary, { nargs = 0 })
+        vim.api.nvim_create_user_command('NeotestClear', run_test_clear_output, { nargs = 0 })
 
         require('neotest').setup {
+            state = { enabled = true },
+            diagnostic = {
+                enabled = true,
+                severity = 0,
+            },
+            floating = {
+                options = {},
+                enabled = true,
+                border = 'rounded',
+                max_height = 0.8,
+                max_width = 0.8,
+                position = 'right',
+            },
             adapters = {
                 require 'neotest-go', -- Registration
             },
@@ -67,9 +92,15 @@ return {
                 expand_errors = true,
                 border = true,
                 mappings = {
+                    expand = 'e',
+                    jump = 'j',
+                    jump_prev = 'k',
+                    jump_next = 'l',
+                    run = 'r',
                     clear_target = 'c',
                     clear_marked = 'm',
                     short = 's',
+                    watch = 'w',
                 },
             },
             run = {
@@ -95,12 +126,12 @@ return {
                 enabled = true,
                 open_on_run = true,
             },
-            watch = {
-                enabled = true,
-                symbol_queries = { 'test', 'example' },
-                patterns = { '*.go' },
-                debounce = 1000,
-            },
+            -- watch = {
+            --     enabled = true,
+            --     symbol_queries = { 'test', 'example' },
+            --     patterns = { '*.go' },
+            --     debounce = 1000,
+            -- },
             statusline = {
                 enable = true,
                 interval = 1000,
