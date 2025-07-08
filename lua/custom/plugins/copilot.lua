e5c425282d039ac5cfff8ab295a17437b68f745e
Amin Karkhi <amk@42960-L.local>
2025-07-08 17:50:45 +0330

    copilot trigger suggestion

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -26,7 +26,7 @@ return {
             },
             suggestion = {
                 enabled = true,
-                auto_trigger = false,
+                auto_trigger = true,
                 hide_during_completion = true,
                 debounce = 75,
                 trigger_on_accept = true,
