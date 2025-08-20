17c6d539b4cd106805028f92a8d3a552cb9787b0
amin.karkhi <amin.karkhi@digikala.com>
2025-08-20 10:08:42 +0330

    update avante

--- lua/custom/plugins/avante.lua
+++ lua/custom/plugins/avante.lua
@@ -3,7 +3,6 @@ return {
     -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
     -- ⚠️ must add this setting! ! !
     build = function()
-        -- conditionally use the correct build system for the current OS
         if vim.fn.has 'win32' == 1 then
             return 'powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false'
         else
@@ -15,12 +14,173 @@ return {
     ---@module 'avante'
     ---@type avante.Config
     opts = {
-        -- add any opts here
-        -- for example
+        ---@alias Provider "claude" | "openai" | "azure" | "gemini" | "cohere" | "copilot" | string
+        ---@type Provider
         provider = 'copilot',
+        ---@alias Mode "agentic" | "legacy"
+        ---@type Mode
+        mode = 'agentic',
         -- providers = {
         --     copilot = {},
         -- },
+        --
+        instructions_file = 'agent.md',
+        behaviour = {
+            auto_suggestions = false, -- Experimental stage
+            auto_set_highlight_group = true,
+            auto_set_keymaps = true,
+            auto_apply_diff_after_generation = false,
+            support_paste_from_clipboard = false,
+            minimize_diff = true, -- Whether to remove unchanged lines when applying a code block
+            enable_token_counting = true, -- Whether to enable token counting. Default to true.
+        },
+        mappings = {
+            --- @class AvanteConflictMappings
+            diff = {
+                ours = 'co',
+                theirs = 'ct',
+                all_theirs = 'ca',
+                both = 'cb',
+                cursor = 'cc',
+                next = ']x',
+                prev = '[x',
+            },
+            suggestion = {
+                accept = '<M-l>',
+                next = '<M-]>',
+                prev = '<M-[>',
+                dismiss = '<C-]>',
+            },
+            jump = {
+                next = ']]',
+                prev = '[[',
+            },
+            submit = {
+                normal = '<CR>',
+                insert = '<C-s>',
+            },
+            cancel = {
+                normal = { '<C-c>', '<Esc>', 'q' },
+                insert = { '<C-c>' },
+            },
+            sidebar = {
+                apply_all = 'A',
+                apply_cursor = 'a',
+                retry_user_request = 'r',
+                edit_user_request = 'e',
+                switch_windows = '<Tab>',
+                reverse_switch_windows = '<S-Tab>',
+                remove_file = 'd',
+                add_file = '@',
+                close = { '<Esc>', 'q' },
+                close_from_input = nil, -- e.g., { normal = "<Esc>", insert = "<C-d>" }
+                next_prompt = ']p',
+                prev_prompt = '[p',
+                toggle_code_window = 'x',
+            },
+            ask = '<leader>aa',
+            new_ask = '<leader>An',
+            edit = '<leader>Ae',
+            refresh = '<leader>Ar',
+            focus = '<leader>Af',
+            stop = '<leader>aS',
+            toggle = {
+                default = '<leader>at',
+                debug = '<leader>ad',
+                selection = '<leader>aC',
+                suggestion = '<leader>as',
+                repomap = '<leader>aR',
+            },
+        },
+        selection = {
+            enabled = true,
+            hint_display = 'delayed',
+        },
+        windows = {
+            ---@type "right" | "left" | "top" | "bottom"
+            position = 'right', -- the position of the sidebar
+            wrap = true, -- similar to vim.o.wrap
+            width = 30, -- default % based on available width
+            sidebar_header = {
+                enabled = true, -- true, false to enable/disable the header
+                align = 'center', -- left, center, right for title
+                rounded = true,
+            },
+            spinner = {
+                editing = {
+                    '⡀',
+                    '⠄',
+                    '⠂',
+                    '⠁',
+                    '⠈',
+                    '⠐',
+                    '⠠',
+                    '⢀',
+                    '⣀',
+                    '⢄',
+                    '⢂',
+                    '⢁',
+                    '⢈',
+                    '⢐',
+                    '⢠',
+                    '⣠',
+                    '⢤',
+                    '⢢',
+                    '⢡',
+                    '⢨',
+                    '⢰',
+                    '⣰',
+                    '⢴',
+                    '⢲',
+                    '⢱',
+                    '⢸',
+                    '⣸',
+                    '⢼',
+                    '⢺',
+                    '⢹',
+                    '⣹',
+                    '⢽',
+                    '⢻',
+                    '⣻',
+                    '⢿',
+                    '⣿',
+                },
+                generating = { '·', '✢', '✳', '∗', '✻', '✽' }, -- Spinner characters for the 'generating' state
+                thinking = { '🤯', '🙄' }, -- Spinner characters for the 'thinking' state
+            },
+            input = {
+                prefix = '> ',
+                height = 20, -- Height of the input window in vertical layout
+            },
+            edit = {
+                border = 'rounded',
+                start_insert = true, -- Start insert mode when opening the edit window
+            },
+            ask = {
+                floating = false, -- Open the 'AvanteAsk' prompt in a floating window
+                start_insert = true, -- Start insert mode when opening the ask window
+                border = 'rounded',
+                ---@type "ours" | "theirs"
+                focus_on_apply = 'ours', -- which diff to focus after applying
+            },
+        },
+        highlights = {
+            ---@type AvanteConflictHighlights
+            diff = {
+                current = 'DiffText',
+                incoming = 'DiffAdd',
+            },
+        },
+        --- @class AvanteConflictUserConfig
+        diff = {
+            autojump = true,
+            ---@type string | fun(): any
+            list_opener = 'copen',
+            --- Override the 'timeoutlen' setting while hovering over a diff (see :help timeoutlen).
+            --- Helps to avoid entering operator-pending mode with diff mappings starting with `c`.
+            --- Disable by setting to -1.
+            override_timeoutlen = 500,
+        },
     },
     dependencies = {
         'nvim-lua/plenary.nvim',
