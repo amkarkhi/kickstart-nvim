26e38a831ed39841940a9373cd3fab48f7676cd0
amin.karkhi <amkarkhi@gmail.com>
2026-06-01 13:58:10 +0330

    update ext.lua

---
+++ after/plugin/ext.lua
@@ -0,0 +1,12 @@
+vim.filetype.add {
+    extension = {
+        gdextension = 'toml',
+    },
+}
+require('nvim-web-devicons').set_icon {
+    gdextension = {
+        icon = '', -- TOML icon
+        color = '#61afef',
+        name = 'Toml',
+    },
+}
